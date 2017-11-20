c42256fc80bf3805fe45367c2e495daf71e0250b
Steve Cirelli <scirelli@gmail.com>
2017-11-20 14:54:02 -0500

    Testing an html cleaner

--- htmlCleaner.js
+++ htmlCleaner.js
@@ -1,4 +1,12 @@
-var sanitizeHtml = require('sanitize-html');
+#!/usr/bin/node
+//var sanitizeHtml = require('sanitize-html');
  
-var dirty = 'some really tacky HTML';
-var clean = sanitizeHtml(dirty);
+var content = [];
+process.stdin.resume();
+process.stdin.on('data', function(buf) { content.push(buf); });
+process.stdin.on('end', function() {
+    console.log(Buffer.concat(content).toString());
+});
+
+//var dirty = 'some really tacky HTML';
+//var clean = sanitizeHtml(dirty);
